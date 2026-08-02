53ce12bdc9dbe412a94efdda383a931439013498
Ahmed Sbai <ahmedclubust@gmail.com>
2026-08-02 23:56:20 +0200

    feat(example): migrate example app to expo sdk 54 with continuous native generation

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { getDefaultConfig } = require('@react-native/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 const { withMetroConfig } = require('react-native-monorepo-config');
 
 const root = path.resolve(__dirname, '..');
@@ -10,7 +10,10 @@ const root = path.resolve(__dirname, '..');
  *
  * @type {import('metro-config').MetroConfig}
  */
-module.exports = withMetroConfig(getDefaultConfig(__dirname), {
+const config = withMetroConfig(getDefaultConfig(__dirname), {
   root,
   dirname: __dirname,
+  conditions: ['source'],
 });
+
+module.exports = config;
